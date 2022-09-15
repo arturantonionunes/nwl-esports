1fb6f5dcf98b001db7d5c545e5569fec6fac7dd3
Artur Nunes <arturantoniojj@gmail.com>
2022-09-14 22:44:44 -0300

    Criação das Rotas da API

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,32 +1,47 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
-const url = "/ads";
 const port = "3333";
 
-app.get(url, (request, response) => {
-  return response.json([
-    {
-      id: 1,
-      name: "Anúncio 1",
-    },
-    {
-      id: 2,
-      name: "Anúncio 2",
-    },
-    {
-      id: 3,
-      name: "Anúncio 3",
-    },
-    {
-      id: 4,
-      name: "Anúncio 4",
-    },
-    {
-      id: 5,
-      name: "Anúncio 5",
-    },
-  ]);
+app.get("/games", (request, response) => {
+	return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+	return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+	//const gameId = request.params.id;
+
+	return response.json([
+		{
+			id: 1,
+			name: "Anúncio 1",
+		},
+		{
+			id: 2,
+			name: "Anúncio 2",
+		},
+		{
+			id: 3,
+			name: "Anúncio 3",
+		},
+		{
+			id: 4,
+			name: "Anúncio 4",
+		},
+		{
+			id: 5,
+			name: "Anúncio 5",
+		},
+	]);
+});
+
+app.get("/games/:id/discord", (request, response) => {
+	//const gameId = request.params.id;
+
+	return response.json([]);
 });
 
 app.listen(port);
